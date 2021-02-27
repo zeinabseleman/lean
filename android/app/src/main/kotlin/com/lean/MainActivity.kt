831e336dad1e66abf69e4cd66e48e70bf0d15688
zeinabseleman <zeinabseleman22@gmail.com>
2021-02-27 09:42:27 +0200

    initial realese

---
+++ android/app/src/main/kotlin/com/lean/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lean
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/costant.dart
@@ -0,0 +1,6 @@
+
+
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF2d6e24);
+const secondColor = Color(0xFFfa2b14);
